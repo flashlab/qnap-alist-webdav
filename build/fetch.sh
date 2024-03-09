3bc9c44c29b8e82bcaac72e6ac9be97b86a4e019
Jeff <zhengjuefei25@163.com>
2024-03-09 16:35:42 +0800

    update

--- build/fetch.sh
+++ build/fetch.sh
@@ -7,7 +7,7 @@ mkdir -p out/extracted
 
 for ARCH in amd64 arm64 arm; do
     echo "Fetching alist_${ARCH}.tgz"
-    wget "https://github.com/alist-org/alist/releases/download/v${QPKG_VER}/alist-linux-musl-${ARCH}.tar.gz" -O "out/tar/alist_${ARCH}.tgz"
+    wget "https://github.com/flashlab/alist/releases/download/v${QPKG_VER}/alist-linux-musl-${ARCH}.tar.gz" -O "out/tar/alist_${ARCH}.tgz"
     tar xvzf "out/tar/alist_${ARCH}.tgz" -C "out/extracted"
     mv "out/extracted/alist" "out/alist-${ARCH}"
 done
